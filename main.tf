388f92f0e98731942e85be960db7bfc01ae137aa
thomasjean-louis <159631593+thomasjean-louis@users.noreply.github.com>
2024-07-08 14:18:11 +0200

    set env variables

--- main.tf
+++ main.tf
@@ -1,5 +1,19 @@
 provider "aws" {
-  region = "us-east-1"
+  region     = "us-east-1"
+  access_key = "TF_VAR_ACCESS_KEY_STATE"
+  secret_key = "TF_VAR_SECRET_ACCESS_KEY_STATE"
+
+}
+
+terraform {
+  backend "s3" {
+    bucket     = "terraform-tjl"
+    key        = "quakejs/terraform.tfstate"
+    region     = "eu-west-3"
+    access_key = "TF_VAR_ACCESS_KEY_INFRA"
+    secret_key = "TF_VAR_SECRET_ACCESS_KEY_INFRA"
+
+  }
 }
 
 
---
+++ variables.tf
@@ -0,0 +1,4 @@
+variable "ACCESS_KEY_STATE" {}
+variable "SECRET_ACCESS_KEY_STATE" {}
+variable "ACCESS_KEY_INFRA" {}
+variable "SECRET_ACCESS_KEY_INFRA" {}
