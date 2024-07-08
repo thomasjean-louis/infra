497f1004922118fd222973fb14eba9dbc33a2b87
thomasjean-louis <159631593+thomasjean-louis@users.noreply.github.com>
2024-07-08 16:38:37 +0200

    update envs

--- main.tf
+++ main.tf
@@ -1,8 +1,5 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = "TF_VAR_ACCESS_KEY_INFRA"
-  secret_key = "TF_VAR_SECRET_ACCESS_KEY_INFRA"
-
 }
 
 terraform {
@@ -10,9 +7,6 @@ terraform {
     bucket     = "terraform-tjl"
     key        = "quakejs/terraform.tfstate"
     region     = "eu-west-3"
-    access_key = "TF_VAR_ACCESS_KEY_STATE"
-    secret_key = "TF_VAR_SECRET_ACCESS_KEY_STATE"
-
   }
 }
 
