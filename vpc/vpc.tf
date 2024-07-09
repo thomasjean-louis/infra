e1072b8e395b657bc3f286602040e0b84c4d0bb6
thomasjean-louis <159631593+thomasjean-louis@users.noreply.github.com>
2024-07-09 09:59:33 +0200

    Update vpc.tf

--- vpc/vpc.tf
+++ vpc/vpc.tf
@@ -64,13 +64,13 @@ resource "aws_route_table" "private_route_table" {
   vpc_id     = aws_vpc.vpc.id
 }
 
-resource "aws_route_table_association" "private_table_association" {
+resource "aws_route_table_association" "private_table_association_a" {
   depends_on     = [aws_route_table.private_route_table, aws_subnet.private_subnet_a]
   subnet_id      = aws_subnet.private_subnet_a.id
   route_table_id = aws_route_table.private_route_table.id
 }
 
-resource "aws_route_table_association" "private_table_association" {
+resource "aws_route_table_association" "private_table_association_b" {
   depends_on     = [aws_route_table.private_route_table, aws_subnet.private_subnet_b]
   subnet_id      = aws_subnet.private_subnet_b.id
   route_table_id = aws_route_table.private_route_table.id
