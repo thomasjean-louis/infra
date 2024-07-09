9ce494a95f6af9875d0052d9a47c73c425e227af
thomasjean-louis <159631593+thomasjean-louis@users.noreply.github.com>
2024-07-09 16:47:46 +0200

    Update ecs.tf

--- ecs/ecs.tf
+++ ecs/ecs.tf
@@ -79,6 +79,22 @@ resource "aws_ecs_task_definition" "game_server_task_definition" {
 resource "aws_security_group" "sg_game_server_ecs" {
   name   = "sg_${var.gameserver_name_container}_ecs"
   vpc_id = var.vpc_id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
 }
 
 resource "aws_vpc_security_group_ingress_rule" "ecs_http" {
