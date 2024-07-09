89677a9629e8b9c0ea08ca3ebcbdb5eac8a9f94b
thomasjean-louis <159631593+thomasjean-louis@users.noreply.github.com>
2024-07-09 16:43:41 +0200

    add https rule

--- ecs/ecs.tf
+++ ecs/ecs.tf
@@ -81,12 +81,20 @@ resource "aws_security_group" "sg_game_server_ecs" {
   vpc_id = var.vpc_id
 }
 
-resource "aws_vpc_security_group_ingress_rule" "ecs" {
+resource "aws_vpc_security_group_ingress_rule" "ecs_http" {
   security_group_id = aws_security_group.sg_game_server_ecs.id
   cidr_ipv4         = "0.0.0.0/0"
-  from_port         = var.game_server_port
+  from_port         = 80
   ip_protocol       = "tcp"
-  to_port           = var.game_server_port
+  to_port           = 80
+}
+
+resource "aws_vpc_security_group_ingress_rule" "ecs_https" {
+  security_group_id = aws_security_group.sg_game_server_ecs.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 443
+  ip_protocol       = "tcp"
+  to_port           = 443
 }
 
 resource "aws_ecs_service" "game_server_service" {
