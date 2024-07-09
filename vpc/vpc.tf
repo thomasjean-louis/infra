9a4bccb638b4e9b8d2ccffd079dbae9ea4f50528
thomasjean-louis <159631593+thomasjean-louis@users.noreply.github.com>
2024-07-09 16:12:57 +0200

    add nat gateway

--- vpc/vpc.tf
+++ vpc/vpc.tf
@@ -125,10 +125,23 @@ resource "aws_route_table_association" "public_table_association_b" {
 
 }
 
+resource "aws_eip" "eip_nat_gateway" {
+
+}
+
+resource "aws_nat_gateway" "nat_gateway" {
+  allocation_id = aws_eip.eip_nat_gateway.id
+  subnet_id     = aws_subnet.public_subnet_a.id
+
+}
 
 resource "aws_route_table" "private_route_table" {
   depends_on = [aws_vpc.vpc]
   vpc_id     = aws_vpc.vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat_gateway.id
+  }
 }
 
 resource "aws_route_table_association" "private_table_association_a" {
@@ -142,3 +155,5 @@ resource "aws_route_table_association" "private_table_association_b" {
   subnet_id      = aws_subnet.private_subnet_b.id
   route_table_id = aws_route_table.private_route_table.id
 }
+
+
