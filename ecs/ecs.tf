f971d783dd80dc61f280c2e2e27b520f50ff1b64
thomasjean-louis <159631593+thomasjean-louis@users.noreply.github.com>
2024-07-10 09:54:50 +0200

    Update ecs.tf

--- ecs/ecs.tf
+++ ecs/ecs.tf
@@ -111,21 +111,6 @@ resource "aws_security_group" "sg_game_server_ecs" {
 
 }
 
-resource "aws_vpc_security_group_ingress_rule" "ecs_http" {
-  security_group_id = aws_security_group.sg_game_server_ecs.id
-  cidr_ipv4         = "0.0.0.0/0"
-  from_port         = 80
-  ip_protocol       = "tcp"
-  to_port           = 80
-}
-
-resource "aws_vpc_security_group_ingress_rule" "ecs_https" {
-  security_group_id = aws_security_group.sg_game_server_ecs.id
-  cidr_ipv4         = "0.0.0.0/0"
-  from_port         = 443
-  ip_protocol       = "tcp"
-  to_port           = 443
-}
 
 resource "aws_ecs_service" "game_server_service" {
   name            = "${var.gameserver_name_container}-service"
