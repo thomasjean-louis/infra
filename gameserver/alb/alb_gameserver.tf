284fa736a60b6ef7c37b488ac25320d69f170455
thomasjean-louis <159631593+thomasjean-louis@users.noreply.github.com>
2024-07-11 10:21:30 +0200

    update gameserver port

--- gameserver/alb/alb_gameserver.tf
+++ gameserver/alb/alb_gameserver.tf
@@ -32,13 +32,6 @@ resource "aws_security_group" "sg_alb" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
   egress {
     from_port   = 80
     to_port     = 80
@@ -46,9 +39,30 @@ resource "aws_security_group" "sg_alb" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  } 
+
   egress {
     from_port   = 443
     to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 27960
+    to_port     = 27960
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  } 
+
+  egress {
+    from_port   = 27960
+    to_port     = 27960
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
