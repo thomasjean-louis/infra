bc3a1b91bf18ee83130e95fd8458c0296f61266c
thomasjean-louis <159631593+thomasjean-louis@users.noreply.github.com>
2024-07-22 10:14:18 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 terraform {
   backend "s3" {
     bucket = "terraform-tjl"
-    key    = "${var.app_name}/terraform.tfstate"
+    key    = "terraform.tfstate"
     region = "eu-west-3"
   }
 }
