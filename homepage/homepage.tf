0cec381d2d207c8e03f50d51b4018a37a72affcd
thomasjean-louis <159631593+thomasjean-louis@users.noreply.github.com>
2024-07-26 16:01:15 +0200

    Update homepage.tf

--- homepage/homepage.tf
+++ homepage/homepage.tf
@@ -103,8 +103,9 @@ resource "aws_amplify_app" "homepage_app" {
 }
 
 resource "aws_amplify_branch" "homepage_branch" {
-  app_id      = aws_amplify_app.homepage_app.id
-  branch_name = var.homepage_branch
+  app_id            = aws_amplify_app.homepage_app.id
+  branch_name       = var.homepage_branch
+  enable_auto_build = true
 
 }
 
