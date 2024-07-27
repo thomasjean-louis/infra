734f390b4872565d7fb12a0a0aac1482df2c33ec
thomasjean-louis <159631593+thomasjean-louis@users.noreply.github.com>
2024-07-27 12:42:18 +0200

    update naming

--- cloud_formation/cloud_formation.tf
+++ cloud_formation/cloud_formation.tf
@@ -38,15 +38,15 @@ resource "aws_cloudformation_stack" "game_server_stack" {
   name = "game-server-stack-${random_string.random_string.result}"
 
   parameters = {
-    vpc_id = var.vpc_id
+    VpcId = var.vpc_id
 
-    hosted_zone_name = var.hosted_zone_name
-    random_string    = random_string.random_string.result
+    HostedZoneName = var.hosted_zone_name
+    RandomString   = random_string.random_string.result
 
-    hosted_zone_id        = data.aws_route53_zone.project_route_zone.zone_id
-    subnet_id_a           = var.subnet_id_a
-    subnet_id_b           = var.subnet_id_b
-    security_group_alb_id = var.security_group_alb_id
+    HostedZoneId       = data.aws_route53_zone.project_route_zone.zone_id
+    SubnetIdA          = var.subnet_id_a
+    SubnetIdB          = var.subnet_id_b
+    SecurityGroupAlbId = var.security_group_alb_id
   }
 
   template_body = file("${path.module}/cloud_formation.yml")
