205963e983530a3c7745e0c76d8538bb90822b8f
thomasjean-louis <159631593+thomasjean-louis@users.noreply.github.com>
2024-08-09 15:49:16 +0200

    Update delete_game_stack.py

--- api_gateway_rest/lambda_game_stacks/delete_game_stack.py
+++ api_gateway_rest/lambda_game_stacks/delete_game_stack.py
@@ -1,33 +1,55 @@
 import os
 import logging
-import boto3
+import boto3 
+import json
 
 logger = logging.getLogger()
 logger.setLevel("INFO")
 
-client = boto3.client('dynamodb')
-dynamodb = boto3.resource("dynamodb")
-table = dynamodb.Table(os.environ["GAME_STACKS_TABLE_NAME"])
 
 def lambda_handler(event, context):
+
+    # Retreive CF_stack_name from dynamodb item id
+    dynamodb_client = boto3.client('dynamodb')
+    table = dynamodb_client.Table(os.environ["GAME_STACKS_TABLE_NAME"])
+
+    item = table.get_item(Key={"id": os.environ["GAME_STACK_ID"]})
+
+    logger.info("stack name : "+item[os.environ["GAME_STACKS_CLOUD_FORMATION_STACK_NAME_COLUMN"]])
+
+    # cloud_formation_client = boto3.client('cloudformation')    
+    # cloud_formation_client.delete_stack(
+    #   StackName=os.environ["GAME_STACKS_TABLE_NAME"]',
+    #   RetainResources=[
+    #     'string',
+    #   ],
+    #   RoleARN='string',
+    #   ClientRequestToken='string',
+    #   DeletionMode='FORCE_DELETE_STACK'    
+    # )
+
+    # Update record in dynamodb to hide it
+   
+
+
     # logger.info(event)
     body = {}
     statusCode = 200
 
-    try:
-        if event['routeKey'] == "DELETE /gamestack":
-            body = table.scan()
-            body = body["Items"]
-            # logger.info(body)
-            responseBody = []
-            for items in body:
-                responseItems = [
-                    {'ID': items['ID'], 'Capacity': float(items['Capacity']), 'ServerLink': items['ServerLink']}]
-                responseBody.append(responseItems)
-            body = responseBody
-    except KeyError:
-        statusCode = 400
-        body = 'Unsupported route: ' + event['routeKey']
+    # try:
+    #     if event['routeKey'] == "DELETE /gamestack":
+    #         body = table.scan()
+    #         body = body["Items"]
+    #         # logger.info(body)
+    #         responseBody = []
+    #         for items in body:
+    #             responseItems = [
+    #                 {'ID': items['ID'], 'Capacity': float(items['Capacity']), 'ServerLink': items['ServerLink']}]
+    #             responseBody.append(responseItems)
+    #         body = responseBody
+    # except KeyError:
+    #     statusCode = 400
+    #     body = 'Unsupported route: ' + event['routeKey']
     body = json.dumps(body)
     res = {
         "statusCode": statusCode,
