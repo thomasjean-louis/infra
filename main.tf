3cd3706cac9bfc95af6dd70339f113d20dfd3473
thomasjean-louis <159631593+thomasjean-louis@users.noreply.github.com>
2024-08-09 11:44:10 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -215,7 +215,7 @@ module "cognito" {
   source                   = "./cognito"
   app_name                 = var.app_name
   default_cognito_username = var.default_cognito_username
-  default_cognito_password = var.default_cognito_username
+  default_cognito_password = var.default_cognito_password
   hosted_zone_id           = local.hosted_zone_id
   subdomain_auth           = var.subdomain_auth
   hosted_zone_name         = var.hosted_zone_name
