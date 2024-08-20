1b1246545ce9cdf16b973e6dd53ddfcfdc250c1b
thomasjean-louis <159631593+thomasjean-louis@users.noreply.github.com>
2024-08-20 12:05:05 +0200

    Update lambda_game_stacks.tf

--- api_gateway_rest/lambda_game_stacks/lambda_game_stacks.tf
+++ api_gateway_rest/lambda_game_stacks/lambda_game_stacks.tf
@@ -184,6 +184,25 @@ resource "aws_iam_role" "lambda_api_service_role" {
   })
 }
 
+resource "aws_iam_role_policy" "ec2_service_policy" {
+  name = "${var.app_name}_lambda_dynamodb_service_${var.deployment_branch}"
+  role = aws_iam_role.lambda_api_service_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "ec2:DescribeVpcs",
+          "ec2:DescribeAccountAttributes"
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+    ]
+  })
+}
+
 resource "aws_iam_role_policy" "dynamodb_service_policy" {
   name = "${var.app_name}_lambda_dynamodb_service_${var.deployment_branch}"
   role = aws_iam_role.lambda_api_service_role.id
